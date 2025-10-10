cd5707ea51151d095458817215cd61d109bbd3d7
Andres B <andresblasiche@gmail.com>
2025-10-10 20:16:44 -0300

    Use case Update Hotel

---
+++ domain/src/use-cases/update-hotel.ts
@@ -0,0 +1,12 @@
+import { Hotel } from "../entities/hotel";
+
+export type UpdateHotelParams = Partial <
+  Pick<Hotel, 'name' | 'address' | 'description'>
+>;
+
+export function updateHotel(hotel:Hotel, updates: UpdateHotelParams): Hotel {
+  return {
+    ...hotel,
+    ...updates,
+  };
+}
