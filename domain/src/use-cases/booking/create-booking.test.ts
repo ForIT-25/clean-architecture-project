7e82e50a28ab7f46d241943c65d210ddcc693b51
Andres B <andresblasiche@gmail.com>
2025-10-15 20:03:52 -0300

    Make use case create booking

---
+++ domain/src/use-cases/booking/create-booking.test.ts
@@ -0,0 +1,27 @@
+import { describe, test, expect } from "vitest";
+import { createBooking } from "./create-booking";
+import { Booking } from "../../entities/booking";
+
+describe("Create Bokkings", () => {
+  test("Create a booking whit it propierties", () => {
+    const booking:Booking = createBooking({
+      id: '1',
+      buyDate: new Date('2025-10-15'),
+      reserveDate: new Date('2025-10-20'),
+      days: 3,
+      idUser: 'Andres',
+      idRoom: '12345678',
+      price: 250,
+    });
+    
+    expect(booking).toEqual({
+      id: '1',
+      buyDate: new Date('2025-10-15'),
+      reserveDate: new Date('2025-10-20'),
+      days: 3,
+      idUser: 'Andres',
+      idRoom: '12345678',
+      price: 250,
+    });
+  });
+});
---
+++ domain/src/use-cases/booking/create-booking.ts
@@ -0,0 +1,21 @@
+interface CreateBookingInput {
+  id: string;
+  buyDate: Date;
+  reserveDate: Date;
+  days: number;
+  idUser: string;
+  idRoom: string;
+  price: number;
+}
+
+export function createBooking(input: CreateBookingInput) {
+  return {
+    id: input.id,
+    buyDate: input.buyDate,
+    reserveDate: input.reserveDate,
+    days: input.days,
+    idUser: input.idUser,
+    idRoom: input.idRoom,
+    price: input.price,
+  };
+};
