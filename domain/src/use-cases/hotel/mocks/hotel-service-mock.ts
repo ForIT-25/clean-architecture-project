a27882cf7fc417165d686b2e5102e64a0c7f1d80
Andres B <andresblasiche@gmail.com>
2025-10-28 19:36:23 -0300

    Make mocks for hotel services

---
+++ domain/src/use-cases/hotel/mocks/hotel-service-mock.ts
@@ -0,0 +1,35 @@
+import { CreateHotelData, Hotel } from "@hotel-project/domain";
+import { vi, Mock } from "vitest";
+
+export const MockHotel:Hotel = {
+  id: 'MOCK-H1',
+  name: 'Mock name',
+  address: 'Mock address 123',
+  description: 'Mock description',
+  rooms: [],
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export type MockedHotelService = {
+  registerHotel: Mock<(params: CreateHotelData) => Promise<Hotel>>;
+  findHotelAll: Mock<() => Promise<Hotel[]>>;
+  findHotelById: Mock<(id: string) => Promise<Hotel | undefined>>;
+  updateHotel: Mock<(id: string, updates: any) => Promise<Hotel | undefined>>;
+  deleteHotel: Mock<(id: string) => Promise<void>>;
+};
+
+export const createMockHotelService = () => {
+  const registerHotelMock = vi.fn(async (params: CreateHotelData) => ({
+    ...MockHotel,
+    ...params,
+  }));
+  
+  return {
+    registerHotel: registerHotelMock,
+    findHotelAll: vi.fn(),
+    findHotelById: vi.fn(),
+    updateHotel: vi.fn(),
+    deleteHotel: vi.fn(),
+  };
+};
